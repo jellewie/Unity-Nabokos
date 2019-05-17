fc6ace6ba6173d9078a8b8ed65ef2f1a81276f32
jellewie <1545223+jellewie@users.noreply.github.com>
2019-05-17 14:49:45 +0200

    added the files

---
+++ Assets/Code/Konami.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Konami : MonoBehaviour
+{
+    public GameObject KonamiFolder;
+
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("Konami", 0) == 1)
+        {
+            KonamiFolder.SetActive(true);
+        } else
+        {
+            KonamiFolder.SetActive(false);
+        }
+    }
+}
